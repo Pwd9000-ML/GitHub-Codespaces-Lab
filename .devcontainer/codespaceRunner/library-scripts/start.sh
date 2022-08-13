e2cbbaf19332c80c56eab7c91b10d5a6eaade159
M L <magiconion_M@hotmail.com>
2022-08-13 12:32:49 +0100

    complete

--- .devcontainer/codespaceRunner/library-scripts/start.sh
+++ .devcontainer/codespaceRunner/library-scripts/start.sh
@@ -7,10 +7,12 @@ GH_TOKEN=$GH_TOKEN
 HOSTNAME=$(hostname)
 RUNNER_SUFFIX="runner"
 RUNNER_NAME="${HOSTNAME}-${RUNNER_SUFFIX}"
+USER_NAME_LABEL=$(git config --get user.name)
+REPO_NAME_LABEL=$(basename `git rev-parse --show-toplevel`)
 
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
-/home/vscode/actions-runner/config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
+/home/vscode/actions-runner/config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}  --labels ${USER_NAME_LABEL},${REPO_NAME_LABEL}
 
 cleanup() {
     echo "Removing runner..."
