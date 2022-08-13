77b7e5e7725806303ad92e251fbf093b9a5a3b50
M L <magiconion_M@hotmail.com>
2022-08-13 10:43:25 +0100

    test

--- .devcontainer/codespaceRunner2/library-scripts/start.sh
+++ .devcontainer/codespaceRunner2/library-scripts/start.sh
@@ -9,16 +9,14 @@ RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
 
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/vscode/actions-runner
-
-./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
+/home/vscode/actions-runner/config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    /home/vscode/actions-runner/config.sh remove --unattended --token ${REG_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
-./run.sh & wait $!
+/home/vscode/actions-runner/run.sh & wait $!
