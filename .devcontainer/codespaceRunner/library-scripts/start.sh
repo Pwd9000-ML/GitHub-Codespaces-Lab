202ceaf9fd1c37f4471095bbeb9942321efce1ce
M L <magiconion_M@hotmail.com>
2022-08-13 13:48:32 +0100

    release

--- .devcontainer/codespaceRunner/library-scripts/start.sh
+++ .devcontainer/codespaceRunner/library-scripts/start.sh
@@ -8,7 +8,7 @@ HOSTNAME=$(hostname)
 RUNNER_SUFFIX="runner"
 RUNNER_NAME="${HOSTNAME}-${RUNNER_SUFFIX}"
 USER_NAME_LABEL=$(git config --get user.name)
-REPO_NAME_LABEL=$(basename `git rev-parse --show-toplevel`)
+REPO_NAME_LABEL="$GH_REPOSITORY"
 
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
