fd44533981934cecbc297db5831b864f736cf6ea
M L <magiconion_M@hotmail.com>
2022-08-13 11:19:48 +0100

    up

--- .devcontainer/codespaceRunner/library-scripts/start.sh
+++ .devcontainer/codespaceRunner/library-scripts/start.sh
@@ -5,7 +5,7 @@ GH_REPOSITORY=$GH_REPOSITORY
 GH_TOKEN=$GH_TOKEN
 
 RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
-RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
+RUNNER_NAME="codespaceRunner-${RUNNER_SUFFIX}"
 
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
