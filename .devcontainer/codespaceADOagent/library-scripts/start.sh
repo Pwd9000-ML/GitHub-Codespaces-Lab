5621e8446cef28b29d6fddee00e8cef4838e5ed0
M L <magiconion_M@hotmail.com>
2022-08-18 17:51:41 +0100

    fix token show

--- .devcontainer/codespaceADOagent/library-scripts/start.sh
+++ .devcontainer/codespaceADOagent/library-scripts/start.sh
@@ -10,6 +10,9 @@ AGENT_SUFFIX="ADO-agent"
 AGENT_NAME="${HOSTNAME}-${AGENT_SUFFIX}"
 ADO_URL="https://dev.azure.com/${ADO_ORG}"
 
+# !!!Ignore sensitive tokens from capabilities!!!
+export VSO_AGENT_IGNORE=ADO_PAT,GH_TOKEN,GITHUB_CODESPACE_TOKEN,GITHUB_TOKEN
+
 /home/vscode/azure-pipelines/config.sh --unattended \
 --agent "${AGENT_NAME}" \
 --url "${ADO_URL}" \
