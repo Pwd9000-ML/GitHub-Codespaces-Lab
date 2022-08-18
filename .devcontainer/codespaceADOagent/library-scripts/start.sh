0fa0f96b8a56f8cb7e047f8e5cd7714bb18e5c24
M L <magiconion_M@hotmail.com>
2022-08-18 17:17:10 +0100

    update

--- .devcontainer/codespaceADOagent/library-scripts/start.sh
+++ .devcontainer/codespaceADOagent/library-scripts/start.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+# Pulled from GitHub Codespace secrets
+ADO_ORG=$ADO_ORG
+ADO_PAT=$ADO_PAT
+ADO_POOL_NAME=$ADO_POOL_NAME
 
+# Derived environment variables
+HOSTNAME=$(hostname)
+AGENT_SUFFIX="ADO-agent"
+AGENT_NAME="${HOSTNAME}-${AGENT_SUFFIX}"
+ADO_URL="https://dev.azure.com/${ADO_ORG}"
+
+/home/vscode/azure-pipelines/config.sh --unattended \
+--agent "${AGENT_NAME}" \
+--url "${ADO_URL}" \
+--auth PAT \
+--token "${ADO_PAT}" \
+--pool "${$ADO_POOL_NAME}" \
+--acceptTeeEula
+
+/home/vscode/azure-pipelines/run.sh
